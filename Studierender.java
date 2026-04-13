6ac1be646db8646d067cb43af344bcd4bd8200c7
sophiaschempp <sophia-schempp@gmx.de>
2026-04-14 00:51:20 +0200

    studierender.java erstellt

---
+++ Studierender.java
@@ -0,0 +1,7 @@
+public class Studierender {
+    private String name;
+    private String matrikelnummer;
+    private String studiengang;
+
+    public void frageStellen(Dozierender dozent, String frage){}
+}
