c56d65ddae82bd66e5e4f7e68eed1cc29e1acfb1
sophiaschempp <sophia-schempp@gmx.de>
2026-04-14 01:13:04 +0200

    aufgabe2 fertig

--- Dozierender.java
+++ Dozierender.java
@@ -1,3 +1,29 @@
 public class Dozierender {
+    private String name;
 
+    public String getName() {
+        return name;
+    }
+
+    private String fakultaet;
+
+    public String getFakultaet() {
+        return fakultaet;
+    }
+
+    private int bueronummer;
+
+    public Dozierender(String name, String fakultaet, int bueronummer) {
+        this.name = name;
+        this.fakultaet = fakultaet;
+        this.bueronummer = bueronummer;
+    }
+
+    public int getBueronummer() {
+        return bueronummer;
+    }
+
+    public void frageBeantworten(Studierender studi, String antwort) {
+        System.out.println("Antwort an " + studi.getName() + ": " + antwort);
+    }
 }
--- Studierender.java
+++ Studierender.java
@@ -1,7 +1,28 @@
 public class Studierender {
     private String name;
-    private String matrikelnummer;
+    public String getName() {
+        return name;
+    }
+
+    private int matrikelnummer;
+    public int getMatrikelnummer() {
+        return matrikelnummer;
+    }
+
     private String studiengang;
 
-    public void frageStellen(Dozierender dozent, String frage){}
+    public String getStudiengang() {
+        return studiengang;
+    }
+
+    public void frageStellen(Dozierender dozent, String frage){
+    System.out.println("Frage an "+dozent.getName()+ ": "+ frage);
+    }
+
+    public Studierender(String name, int matrikelnummer, String studiengang) {
+        this.name = name;
+        this.matrikelnummer = matrikelnummer;
+        this.studiengang = studiengang;
+    }
+    
 }
